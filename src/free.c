083efaedfc5176b772fc3a57df703eed7a9b1710
Lucas Duchemin <lduchemi@c2r7p1.42lehavre.fr>
2024-10-09 17:48:02 +0200

    fix empty color

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: lduchemi <lduchemi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 15:37:05 by lduchemi          #+#    #+#             */
-/*   Updated: 2024/07/22 15:56:51 by lduchemi         ###   ########.fr       */
+/*   Updated: 2024/10/09 17:40:43 by lduchemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	free_all(t_data *data)
 {
-	if (data->init)
+	if (data->init && (data->verif[5] == 1 && data->verif[6] == 1))
 		mlx_destroy_image(data->mlx_ptr, data->img_ptr);
 	if (data->win_ptr)
 	{
